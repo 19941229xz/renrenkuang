2a71339cfe863f3b57269368bc99b7938512b334
hc12345678 <1737957810@qq.com>
2019-08-02 16:40:30 +0800

    Initial commit

---
+++ src/main/java/com/example/renrenkuang/RenrenkuangApplication.java
@@ -0,0 +1,13 @@
+package com.example.renrenkuang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenrenkuangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RenrenkuangApplication.class, args);
+    }
+
+}
